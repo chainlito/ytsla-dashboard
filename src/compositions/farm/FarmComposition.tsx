9d58e24dc313aac08293d5a5e20bbc797c498337
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-09-21 11:13:42 -0300

    farm apy

--- src/compositions/farm/FarmComposition.tsx
+++ src/compositions/farm/FarmComposition.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import Config from 'config';
@@ -11,6 +11,7 @@ import PoolCard from './PoolCard';
 import Farm1Icon from 'assets/img/token-stake.png';
 import Farm2Icon from 'assets/img/token-uni.png';
 import Farm3Icon from 'assets/img/token-lp.png';
+import { coingeckoclient, web3client } from 'lib';
 
 interface StateFromProps {
   account: ReturnType<typeof selectAccount>;
@@ -21,6 +22,36 @@ interface OwnProps {}
 type Props = StateFromProps & DispatchFromProps & OwnProps;
 
 const FarmComposition = () => {
+  const [tokenPrice, setTokenPrice] = React.useState<number>(0);
+  const [pool1APY, setPool1APY] = React.useState<number>(0);
+  const [pool2APY, setPool2APY] = React.useState<number>(0);
+  const [pool3APY, setPool3APY] = React.useState<number>(0);
+  useEffect(() => {
+    coingeckoclient.getYtslaPrice().then(res => setTokenPrice(res));
+  });
+  useEffect(() => {
+    if (tokenPrice > 0) {
+      coingeckoclient.getMemePrice().then(price => {
+        web3client.poolGetRewardRate(web3client.pool1Contract).then(res => {
+            const roi = res * tokenPrice / Math.pow(10, 28) / price * 86400 * 365 * 100;
+            setPool1APY(roi);
+        });
+      });
+
+      coingeckoclient.getUniPrice().then(price => {
+        web3client.poolGetRewardRate(web3client.pool2Contract).then(res => {
+            const roi = res * tokenPrice / Math.pow(10, 18) / price * 86400 * 365 * 100;
+            setPool2APY(roi);
+        });
+      });
+
+      web3client.poolGetRewardRate(web3client.pool3Contract).then(res => {
+        const roi = res * tokenPrice / Math.pow(10, 18) / 90.43 * 86400 * 365 * 100;
+        setPool3APY(roi);
+    });
+    }
+  }, [tokenPrice]);
+
   return (
     <React.Fragment>
       <Header />
@@ -37,9 +68,9 @@ const FarmComposition = () => {
             </div>
           </div>
           <div className='center-h wp-100 mt-30 home-container'>
-            <PoolCard stakingToken={Config.MemeToken} picture={Farm1Icon} poolUrl='/farm-meme' />
-            <PoolCard stakingToken={Config.UniToken} picture={Farm2Icon} poolUrl='/farm-uni' />
-            <PoolCard stakingToken={Config.UniLpToken} picture={Farm3Icon} poolUrl='/farm-lp' />
+            <PoolCard stakingToken={Config.MemeToken} picture={Farm1Icon} poolUrl='/farm-meme' apy={pool1APY} rewardPercent={16.33} />
+            <PoolCard stakingToken={Config.UniToken} picture={Farm2Icon} poolUrl='/farm-uni' apy={pool2APY} rewardPercent={16.33} />
+            <PoolCard stakingToken={Config.UniLpToken} picture={Farm3Icon} poolUrl='/farm-lp' apy={pool3APY} rewardPercent={67.34} />
           </div>
         </div>
       </Container>
--- src/compositions/farm/PoolCard.tsx
+++ src/compositions/farm/PoolCard.tsx
@@ -7,11 +7,13 @@ interface OwnProps {
   stakingToken: any;
   picture: string;
   poolUrl: string;
+  apy: number;
+  rewardPercent: number;
 }
 
 type Props = OwnProps & RouteComponentProps;
 
-const PoolCard: React.FC<Props> = ({ stakingToken, picture, poolUrl, history }: Props) => {
+const PoolCard: React.FC<Props> = ({ stakingToken, picture, poolUrl, apy, rewardPercent, history }: Props) => {
   return (
     <Card className='card card-h medium transparent'>
       <CardContent>
@@ -20,13 +22,19 @@ const PoolCard: React.FC<Props> = ({ stakingToken, picture, poolUrl, history }: 
             <img className="logo-image" src={picture} alt='icon' />
           </div>
           <div className='center-h'>
-            <h2>{`${stakingToken.name} Farm`}</h2>
+            <h2 className='mt-10 mb-10'>{`${stakingToken.name} Farm`}</h2>
           </div>
-          <div className='center-h mb-10'>
+          <div className='center-h'>
             <span className='text-small'>{`Deposit ${stakingToken.symbol}`}</span>
           </div>
           <div className='center-h mb-20'>
             <span className='text-small'>{`Earn ${Config.Token.symbol}`}</span>
+          </div>
+          <div className='center-h'>
+            <span className='text-error'>{`${apy.toFixed(2)}% APY`}</span>
+          </div>
+          <div className='center-h'>
+            <span className='text-small'>{`${rewardPercent.toFixed(2)}% of Rewards`}</span>
           </div>
         </div>
         <div className='section'>
